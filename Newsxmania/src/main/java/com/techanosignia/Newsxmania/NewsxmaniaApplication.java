15a5f551efefd8a68a083b771ad64fbb1efdda25
PRAVINMARUTIDHANGAPURE <89832229+PRAVINMARUTIDHANGAPURE@users.noreply.github.com>
2021-09-01 12:03:25 +0530

    Add files via upload
    first commit

---
+++ Newsxmania/src/main/java/com/techanosignia/Newsxmania/NewsxmaniaApplication.java
@@ -0,0 +1,13 @@
+package com.techanosignia.Newsxmania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsxmaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsxmaniaApplication.class, args);
+	}
+
+}
---
+++ Newsxmania/src/test/java/com/techanosignia/Newsxmania/NewsxmaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techanosignia.Newsxmania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsxmaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
